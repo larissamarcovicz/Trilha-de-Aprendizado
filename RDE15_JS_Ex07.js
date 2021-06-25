c616dab8732cabfdfcbe344aca148b3159b6c6ea
Larissa de Fátima Marcovicz <82007618+larissamarcovicz@users.noreply.github.com>
2021-06-25 20:28:46 -0300

    RDE 15 16 17 JS CSS

---
+++ RDE15_JS_Ex07.js
@@ -0,0 +1,7 @@
+function somar(){
+    var n1,n2, resultado
+    n1= parseFloat(document.getElementById("n1").value)
+    n2= parseFloat(document.getElementById("n2").value)
+    resultado=n1+n2
+    document.getElementById("resultadio").innerHTML = `O resultado da soma é ${resultado}`
+}
